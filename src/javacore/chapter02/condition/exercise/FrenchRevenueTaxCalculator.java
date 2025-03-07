15bcc6c800c84ec7176469389800e30ea7a927cf
steve <steve85.declercq@gmail.com>
2025-03-07 18:48:27 +0100

    coinChangeV1

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -3,59 +3,40 @@ package javacore.chapter02.condition.exercise;
 public class FrenchRevenueTaxCalculator {
     public static void main(String [] args) {
         //
-        double salary = 28899;
-        boolean cadre = true;
-        boolean ouvrier = false;
+        double salary = 34000;
+        double salaryBeforTaxe = salary;
         double abattement = 0.9;
+        salary *= abattement;
+        double taxPayed = 0;
+        double taxe41 = (180294 - 83824) * 0.41;
+        double taxe30 = (83823 - 29316) * 0.3;
+        double taxe11 = (29315 - 11498) * 0.11;
 
-        if (ouvrier)
-        {
-            salary*=0.77*abattement;
-            System.out.println("En tant qu'ouvrier ");
-            if (salary > 177106) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary * 0.55 + " ce qui fait que vous avez payé "
-                        + salary * 0.45 + " d'impot(s), soit 45%");
-            }
-            if (salary <= 177106 && salary >= 82342) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary * 0.59 + " ce qui fait que vous avez payé "
-                        + salary * 0.41 + " d'impot(s) soit 41%");
-            }
-            if (salary < 82342 && salary > 28798) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary * 0.70 + " ce qui fait que vous avez payé "
-                        + salary * 0.3 + " d'impot(s) soit 30%");
-            }
-            if (salary < 28798 && salary > 11264) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary * 0.89 + " ce qui fait que vous avez payé "
-                        + salary * 0.11 + " d'impot(s) soit 11%");
-            }
-            if (salary < 11264) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary + " ce qui fait que vous avez payé "
-                        + salary * 0 + " d'impot(s) soit 0%");
-            }
+        if (salary > 180294) {
+            taxPayed += (salary - 180295) * 0.45;
+            taxPayed += taxe41;
+            taxPayed += taxe30;
+            taxPayed += taxe11;
         }
-        if (cadre)
-        {
-            salary*=0.75*abattement;
-            if (salary > 177106) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary * 0.55 + " ce qui fait que vous avez payé "
-                        + salary * 0.45 + " d'impot(s), soit 45%");
-            }
-            if (salary <= 177106 && salary >= 82342) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary * 0.59 + " ce qui fait que vous avez payé "
-                        + salary * 0.41 + " d'impot(s) soit 41%");
-            }
-            if (salary < 82342 && salary > 28798) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary * 0.70 + " ce qui fait que vous avez payé "
-                        + salary * 0.3 + " d'impot(s) soit 30%");
-            }
-            if (salary < 28798 && salary > 11264) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary * 0.89 + " ce qui fait que vous avez payé "
-                        + salary * 0.11 + " d'impot(s) soit 11%");
-            }
-            if (salary < 11264) {
-                System.out.println("Pour " + salary + " imposable vous touchez " + salary + " ce qui fait que vous avez payé "
-                        + salary * 0 + " d'impot(s) soit 0%");
-            }
+
+        if (salary <= 180294 && salary >= 83824) {
+            taxPayed += (salary - 83824) * 0.41;
+            taxPayed += taxe30;
+            taxPayed += taxe11;
         }
+
+        if (salary < 83824 && salary > 29316) {
+            taxPayed += (salary - 29316) * 0.3;
+            taxPayed += taxe11;
+        }
+
+        if (salary < 29316 && salary > 11498) {
+            taxPayed += (salary - 11498) * 0.11;
+        }
+
+        System.out.println("Vous avez touché " + salaryBeforTaxe + " et " + salary + " était imposable.\nSur ce vous avez payé "
+                + taxPayed);
+
+
     }
 }
