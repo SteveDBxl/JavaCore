c40d4a16f5cdcc978e19d85995514ec0a3905105
steve <steve85.declercq@gmail.com>
2025-04-01 14:04:34 +0200

    Merge remote-tracking branch 'origin/master'

--- src/javacore/chapter03/loop/exercise/PrimeNumberSearcherV1.java
+++ src/javacore/chapter03/loop/exercise/PrimeNumberSearcherV1.java
@@ -3,27 +3,51 @@ package javacore.chapter03.loop.exercise;
 public class PrimeNumberSearcherV1 {
 
     public static void main(String[] args){
-
-        int number = 2;
+/*
+        //representera le nombre premier
+        int nombrePremier = 2;
+        //permet de déterminer combien de nb premier nous désirons afficher
         int nombreDePremier = 100;
+        //combien de nb premier on déjà été affiché
         int compteurDePremier = 1;
+        //détermine si nous avons affaire à un nb premier et l'afficher si tel est le cas
         boolean estPremier= true;
 
-        for(int nbIteration = 0; compteurDePremier <= 100; nbIteration++){
-            for(int diviseur = 2; diviseur < number; diviseur++){
-                if( number % diviseur == 0 ){
+
+        //nb itération permet de savoir combien de nombre nous avons tes
+        for(int nbIteration = 0; compteurDePremier <= nombreDePremier; nbIteration++){
+            estPremier = true;
+            for(int potentielDiviseur = 2; potentielDiviseur < nombrePremier; potentielDiviseur++){
+                if( nombrePremier % potentielDiviseur == 0 ){
                     estPremier = false;
                     break;
                 }
-                else {
-                    estPremier = true;
-                }
             }
             if( estPremier ){
-                System.out.println(number + " est premier : numéro d'itération " + compteurDePremier ) ;
+                System.out.println(nombrePremier + " est premier : numéro d'itération " + compteurDePremier ) ;
                 compteurDePremier++;
             }
-            number++;
+            nombrePremier++;
+        }
+*/
+        //Version plus économ en calcul
+
+        int nbDePremierVoulu = 100;
+        boolean isEven = true;
+        for(int potentielPremier = 2; nbDePremierVoulu > 0; potentielPremier++ ){
+            isEven = true;
+            //potentielPremier /2 car on aurait déjà trouvé un candidat dans la première moitié
+            for(int potentielDiviseur = 2; potentielDiviseur <= potentielPremier / 2; potentielDiviseur++){
+
+                if(potentielPremier % potentielDiviseur == 0){
+                    isEven = false;
+                    break;
+                }
+            }
+            if(isEven){
+                System.out.println(potentielPremier + " est premier");
+                nbDePremierVoulu--;
+            }
         }
 
     }
