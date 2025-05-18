3d7935e4abd316f7d220a942686f0107d070f128
steve <steve85.declercq@gmail.com>
2025-05-19 00:30:10 +0200

    Merge remote-tracking branch 'origin/master'

--- src/javacore/chapter04/function/exercise/RecursiveFibonacci.java
+++ src/javacore/chapter04/function/exercise/RecursiveFibonacci.java
@@ -2,7 +2,7 @@ package javacore.chapter04.function.exercise;
 
 public class RecursiveFibonacci {
 
-    public static void fiboRacursive(long number1, long number2, int indice){
+    /*public static void fiboRacursive(long number1, long number2, int indice){
 
         System.out.print(number1 + ", " + number2 + ", ");
         number1 = number1 + number2;
@@ -14,5 +14,25 @@ public class RecursiveFibonacci {
 
     public static void main (String args[]){
         fiboRacursive(0,1, 1);
+    }
+}*/
+
+    /*******************************************************/
+    public static int fiboRacursive( int indice){
+        if( indice <= 1){
+            return indice;
+        }
+        return fiboRacursive(indice - 1) + fiboRacursive(indice - 2);
+
+
+    }
+
+    public static void main (String args[]){
+        int targetNumber = 30;
+        for(int count = 0; count < targetNumber; count++){
+            System.out.print( (count >= targetNumber)?fiboRacursive(count) : (fiboRacursive( count ) + ", "));
+        }
+
+
     }
 }
--- src/javacore/chapter04/function/exercise/RecursiveFunction.java
+++ src/javacore/chapter04/function/exercise/RecursiveFunction.java
@@ -3,11 +3,10 @@ package javacore.chapter04.function.exercise;
 public class RecursiveFunction {
     static void countToThousand(int indice){
         System.out.println(indice);
-        if(indice < 1000) {
-            countToThousand(indice + 1);
+        if(indice >= 1000) {
             return;
         }
-        return;
+        countToThousand(indice + 1);
     }
     public static void main(String args[]){
         countToThousand(1);
