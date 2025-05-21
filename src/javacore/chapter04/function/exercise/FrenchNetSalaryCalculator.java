2c46a848b875c776b6873695f81730f5a2b7e5c1
steve <steve85.declercq@gmail.com>
2025-05-21 17:32:21 +0200

    Merge remote-tracking branch 'origin/master'

--- src/javacore/chapter04/function/exercise/FrenchNetSalaryCalculator.java
+++ src/javacore/chapter04/function/exercise/FrenchNetSalaryCalculator.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class FrenchNetSalaryCalculator {
 
+    static final double TAX_41 = (180294 - 83824) * 0.41;
+    static final double TAX_3 = (83823 - 29316) * 0.3;
+    static final double TAX_11 = (29315 - 11498) * 0.11;
     static double calculateAnnualGrossSalary(double hourlyGrossSalary, int numberHourAWeek, int numberWeekAYear){
 
         return (hourlyGrossSalary * numberHourAWeek * numberWeekAYear) ;
@@ -11,58 +14,43 @@ public class FrenchNetSalaryCalculator {
 
     static double applySocialCharges(double annualGrossSalary, double socialChargePercentage){
 
-        return annualGrossSalary * (1 - socialChargePercentage);
+        return annualGrossSalary * socialChargePercentage;
     }
 
     static double applyTaxAllowance(double annualNetSalary, double taxAllowancePercentage){
-        return (annualNetSalary *  taxAllowancePercentage);
+        return annualNetSalary *  taxAllowancePercentage;
     }
 
     static double applyRevenueTax(double annualNetSalary){
-        //variable mise en final
-        double taxe41 = (180294 - 83824) * 0.41;
-        double taxe30 = (83823 - 29316) * 0.3;
-        double taxe11 = (29315 - 11498) * 0.11;
+
         double taxPayed = 0;
 
         if (annualNetSalary > 180294) {
             taxPayed += (annualNetSalary - 180295) * 0.45;
-            taxPayed += taxe41;
-            taxPayed += taxe30;
-            taxPayed += taxe11;
-
-            return annualNetSalary - taxPayed;
+            annualNetSalary = annualNetSalary - (annualNetSalary - 180295);
         }
 
-        if (annualNetSalary <= 180294 && annualNetSalary >= 83824) {
+        if (annualNetSalary >= 83824) {
             taxPayed += (annualNetSalary - 83824) * 0.41;
-            taxPayed += taxe30;
-            taxPayed += taxe11;
-
-            return annualNetSalary - taxPayed;
+            annualNetSalary = annualNetSalary - (annualNetSalary - 180294);
         }
 
-        if (annualNetSalary < 83824 && annualNetSalary > 29316) {
+        if (annualNetSalary >= 29316) {
             taxPayed += (annualNetSalary - 29316) * 0.3;
-            taxPayed += taxe11;
-
-            return annualNetSalary - taxPayed;
+            annualNetSalary = annualNetSalary - (annualNetSalary - 83824);
         }
-        if (annualNetSalary < 29316 && annualNetSalary > 11498) {
+        if (annualNetSalary >= 11498) {
             taxPayed += (annualNetSalary - 11498) * 0.11;
 
-            return annualNetSalary - taxPayed;
         }
 
-        return annualNetSalary;
+        return taxPayed;
 
     }
     public static void main(String[] args){
 
         int numberHourAWeek;
         int numberWeekAYear;
-        double salary = 34000;
-        double salaryBeforTaxe = salary;
         double tauxAbattement = 1 - 0.1;
         double cotisationSocialOuvrier = 1 - 0.23;
         double cotisationSocialCadre =  1 - 0.25;
@@ -89,7 +77,7 @@ public class FrenchNetSalaryCalculator {
         afterTaxSalary = applyTaxAllowance(annualNetSalary, tauxAbattement);
         System.out.println(afterTaxSalary);
 
-        System.out.println(applyRevenueTax(afterTaxSalary));
+        System.out.println("tax : " + applyRevenueTax(afterTaxSalary));
 
         //System.out.println("Vous avez touché " + salaryBeforTaxe + " et " + salary + " était imposable.\nSur ce vous avez payé "
          //       );
